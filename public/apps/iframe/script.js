97255af5c79cd3b79e241f28a4cd2667b9ccf9d8
drmatt13 <Matthew.Sweeney001@gmail.com>
2020-09-30 20:49:43 -0400

    cleanslate

--- public/apps/iframe/script.js
+++
@@ -1,6 +0,0 @@
-
-for (var i = 0; i < 2; i++) {
-  let div = document.createElement('div');
-  div.classList.add('div');
-  document.body.appendChild(div);
-}
--- public/scripts/shopScripts/shopHome.js
+++
@@ -1,7 +0,0 @@
-const itemContainers = document.querySelectorAll(".item-container");
-for (let i=0; i<itemContainers.length; i++) {
-  itemContainers[i].addEventListener("click", () => {
-      window.location.href = `/shop/${itemContainers[i].getAttribute('link')}`;
-    });
-}
-
